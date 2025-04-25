563f4110659bd05d7872e0646a964c27cb91f24d
Mai Son Viet <sonviet7052006@gamil.com>
2025-04-25 09:54:17 +0700

    buoi3

--- booking page/main.js
+++ booking page/main.js
@@ -3,20 +3,22 @@ let index = localStorage.getItem("index");
 let p = document.getElementById("user_name");
 p.innerText = `Hello ${account[index].username}`;
 function showForm() {
-  const form = document.getElementById("buttonId");
   const overlay = document.getElementById("overlay");
   overlay.style.display = "flex";
   overlay.style.justifyContent = "center";
+  overlay.style.zIndex="999"
 }
+
+let find = false;
 
 function inputDatabase(event) {
   event.preventDefault();
-  let class_gym = document.getElementById("class").value;
-  let date = document.getElementById("date").value;
-  let time = document.getElementById("time").value;
-  let name = document.getElementById("name").value;
-  console.log(name);
-  let email = document.getElementById("email").value;
+  let class_gym = document.getElementById("class").value.trim()
+  let date = document.getElementById("date").value.trim()
+  let time = document.getElementById("time").value.trim()
+  let name = document.getElementById("name").value.trim()
+  let email = document.getElementById("email").value.trim()
+  
   let class_user = {
     class_gym: class_gym,
     date: date,
@@ -24,11 +26,25 @@ function inputDatabase(event) {
     name: name,
     email: email,
   };
-  account[index].class_user.push(class_user);
+  
+  if (find) {
+    account[index].class_user[index_class] = class_user;
+  } else {
+    if (class_gym === "" || date === "" || time === "" || name === "" || email === "") {
+      alert("Dữ liệu không được bỏ trống, hãy nhập cho đầy đủ!");
+      return;
+    }
+    if (account[index].class_user.some(user => user.email === email || (user.name === name && user.time === time))) {
+      alert("Email hoặc tên với khung giờ này đã tồn tại. Vui lòng kiểm tra lại!");
+      return;
+    }
+    account[index].class_user.push(class_user);
+  }
+
   localStorage.setItem("account", JSON.stringify(account));
   renderData(account[index].class_user);
-  event.preventDefault();
   document.getElementById("overlay").style.display = "none";
+  
 }
 
 function hide_form(event) {
@@ -47,8 +63,8 @@ function pagin() {
   renderData(newArr);
   btn();
 }
-function i_step(i){
-  currPage=i
+function i_step(i) {
+  currPage = i;
   pagin();
   btn();
 }
@@ -61,19 +77,55 @@ function btn() {
 }
 pagin();
 
+function showDeleteModal() {
+  document.getElementById("modal_overlay").style.display = "flex";
+}
+
+function hideDelete() {
+  document.getElementById("modal_overlay").style.display = "none";
+}
+
+function delete_index(realIndex) {
+  if (confirm("Bạn có muốn xóa ?")) {
+    account[index].class_user.splice(realIndex, 1);
+    localStorage.setItem("account", JSON.stringify(account));
+    totalPage = Math.ceil(account[index].class_user.length / perPage);
+    if (currPage > totalPage) currPage = totalPage;
+    pagin();
+    hideDelete();
+  }
+}
+let index_class;
+function editEl(i) {
+  showForm();
+  document.getElementById("class").value = account[index].class_user[i].class_gym;
+  document.getElementById("date").value = account[index].class_user[i].date;
+  document.getElementById("time").value = account[index].class_user[i].time;
+  document.getElementById("name").value = account[index].class_user[i].name;
+  document.getElementById("email").value = account[index].class_user[i].email;
+
+  find = true;
+  index_class = i;
+
+  document.getElementById("change").innerText = "Lưu chỉnh sửa";
+}
+
+
 function renderData(arr) {
   const tbody = document.querySelector("tbody");
   tbody.innerHTML = "";
-  arr.forEach((el, index) => {
+  arr.forEach((el, i) => {
     let tr = document.createElement("tr");
     tr.innerHTML = `
-    <td>${el.class_gym}</td>
-    <td>${el.date}</td>
-    <td>${el.time}</td>
-    <td>${el.name}</td>
-    <td>${el.email}</td>
-    <button id ="edit_">Sửa</button>
-    <button id ="delete_">Xóa</button>
+      <td>${el.class_gym}</td>
+      <td>${el.date}</td>
+      <td>${el.time}</td>
+      <td>${el.name}</td>
+      <td>${el.email}</td>
+      <td>
+        <button onclick="editEl(${i})" id="edit_">Sửa</button>
+        <button onclick="showDeleteModal(${i})" id="delete_">Xóa</button>
+      </td>
     `;
     tbody.appendChild(tr);
   });
